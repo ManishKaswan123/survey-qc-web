431a9135487b688db61f748b23ba8d6416b17535
SatyamRana50 <194532@nith.ac.in>
2024-09-14 21:45:51 +0530

    count added in section

--- src/app/pages/module/section/section.component/section.table.tsx
+++ src/app/pages/module/section/section.component/section.table.tsx
@@ -4,7 +4,14 @@ import {FaRocket} from 'react-icons/fa'
 import {SectionTableProps} from '../section.interfaces'
 import {statusColors, statusMap} from 'sr/constants/status'
 
-const SectionTable: React.FC<SectionTableProps> = ({sectionData, receivedData, surveyId}) => {
+const SectionTable: React.FC<SectionTableProps> = ({
+  sectionData,
+  receivedData,
+  surveyId,
+  programId,
+  totalAttemptedQuestionsMap,
+  totalQuestionsMap,
+}) => {
   const navigate = useNavigate()
 
   return (
@@ -13,22 +20,14 @@ const SectionTable: React.FC<SectionTableProps> = ({sectionData, receivedData, s
         <table className='min-w-full leading-normal'>
           <thead>
             <tr>
-              {/* <th className='py-5 bg-[#265B91] text-center text-xs font-semibold text-gray-50 uppercase tracking-wider border-r'>
-                Display Order
-              </th> */}
               <th className='py-5 bg-[#265B91] text-center text-xs font-semibold text-gray-50 uppercase tracking-wider border-r'>
                 Section Code
               </th>
               <th className='py-5 bg-[#265B91] text-center text-xs font-semibold text-gray-50 uppercase tracking-wider border-r'>
                 Section Name
               </th>
-
-              {/* <th className='py-5 bg-[#265B91] text-center text-xs font-semibold text-gray-50 uppercase tracking-wider border-r'>
-                Description
-              </th> */}
-
               <th className='py-5 bg-[#265B91] text-center text-xs font-semibold text-gray-50 uppercase tracking-wider border-r'>
-                Count
+                Count (Attempted / Total)
               </th>
               <th className='py-5 bg-[#265B91] text-center text-xs font-semibold text-gray-50 uppercase tracking-wider border-r'>
                 Status
@@ -39,56 +38,46 @@ const SectionTable: React.FC<SectionTableProps> = ({sectionData, receivedData, s
             </tr>
           </thead>
           <tbody>
-            {sectionData.map((section, index) => (
-              <tr key={section._id} className='odd:bg-white even:bg-blue-50'>
-                {/* <td className='py-5 text-center border-b border-gray-200 text-sm border-r'>
-                  <div className='flex justify-center items-center'>
-                    <span>{item.ulbId}</span>
-                    <FaInfoCircle className='ml-2 text-blue-600' />
-                  </div>
-                </td> */}
-                {/* <td className='py-5 text-center border-b border-gray-200 text-sm border-r'>
-                  <p>{section.displayOrder}</p>
-                </td> */}
-                <td className='py-5 text-center border-b border-gray-200 text-sm border-r'>
-                  <p>{section.sectionCode}</p>
-                </td>
-                <td className='py-5 text-center border-b border-gray-200 text-sm border-r'>
-                  <p>{section.sectionName}</p>
-                </td>
+            {sectionData.map((section) => {
+              // Get the total questions and attempted questions for the section
+              const totalQuestions = totalQuestionsMap[section._id] || 0
+              const attemptedQuestions = totalAttemptedQuestionsMap[section._id] || 0
 
-                {/* <td className='py-5 text-center border-b border-gray-200 text-sm border-r'>
-                  <p>{section.description}</p>
-                </td> */}
-                <td className='py-5 text-center border-b border-gray-200 text-sm border-r'>
-                  <p>{}</p>
-                </td>
-                {/* <td className='py-5 text-center border-b border-gray-200 text-sm border-r'>
-                  <p>{`${survey.qaId?.firstName || ''} ${' '} ${survey.qaId?.lastName || ''}`}</p>
-                </td> */}
-                <td className='py-5 text-center border-b border-gray-200 text-sm border-r'>
-                  <span
-                    className={`font-bold text-md ${
-                      statusColors[receivedData[section._id] || 'yetToStart']
-                    }`}
-                  >
-                    {statusMap.get(receivedData[section._id] || 'yetToStart')}
-                  </span>
-                </td>
-
-                <td className='py-5 text-center border-b border-gray-200 text-sm border-r'>
-                  <div className='flex justify-center items-center'>
-                    <FaRocket
-                      className='text-blue-800 hover:cursor-pointer'
-                      size={20}
-                      onClick={() => {
-                        navigate(`/question/?surveyId=${surveyId}`)
-                      }}
-                    />
-                  </div>
-                </td>
-              </tr>
-            ))}
+              return (
+                <tr key={section._id} className='odd:bg-white even:bg-blue-50'>
+                  <td className='py-5 text-center border-b border-gray-200 text-sm border-r'>
+                    <p>{section.sectionCode}</p>
+                  </td>
+                  <td className='py-5 text-center border-b border-gray-200 text-sm border-r'>
+                    <p>{section.sectionName}</p>
+                  </td>
+                  <td className='py-5 text-center border-b border-gray-200 text-sm border-r'>
+                    <p>{`${attemptedQuestions} / ${totalQuestions}`}</p>{' '}
+                    {/* Count in "Attempted / Total" format */}
+                  </td>
+                  <td className='py-5 text-center border-b border-gray-200 text-sm border-r'>
+                    <span
+                      className={`font-bold text-md ${
+                        statusColors[receivedData[section._id] || 'yetToStart']
+                      }`}
+                    >
+                      {statusMap.get(receivedData[section._id] || 'yetToStart')}
+                    </span>
+                  </td>
+                  <td className='py-5 text-center border-b border-gray-200 text-sm border-r'>
+                    <div className='flex justify-center items-center'>
+                      <FaRocket
+                        className='text-blue-800 hover:cursor-pointer'
+                        size={20}
+                        onClick={() => {
+                          navigate(`/question/?surveyId=${surveyId}`)
+                        }}
+                      />
+                    </div>
+                  </td>
+                </tr>
+              )
+            })}
           </tbody>
         </table>
       </div>
