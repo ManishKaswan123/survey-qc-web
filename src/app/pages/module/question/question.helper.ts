2b49810487ebb140f2064ede26d1142814461f35
manish <manishkaswan88@gmail.com>
2024-09-16 17:19:43 +0530

    Answer update is connected to backend

--- src/app/pages/module/question/question.helper.ts
+++ src/app/pages/module/question/question.helper.ts
@@ -1,6 +1,10 @@
-
 import {get, patch, post, remove} from 'sr/utils/axios'
-import {AnswerResponse, FilterProps, QuestionResponse} from './question.interface'
+import {
+  AnswerResponse,
+  FilterAnswerProps,
+  FilterProps,
+  QuestionResponse,
+} from './question.interface'
 
 const filterPayload = (payload: FilterProps) => {
   return Object.fromEntries(
@@ -93,5 +97,28 @@ export const deleteQuestion = async (payload: string): Promise<boolean> => {
     return true
   } catch (e: any) {
     throw new Error(e.message)
+  }
+}
+
+export const UpdateAnswers = async (
+  answerId: string,
+  // payload?: FilterAnswerProps
+  payload?: any
+): Promise<any> => {
+  const filteredPayload = filterPayload(payload ?? {})
+
+  try {
+    const res = await patch<any>(`/answer/${answerId}`, filteredPayload)
+
+    // console.log('res of answer is this :-', res)
+    if (res && res.status == 'success') {
+      return res // Return the fetched data
+    } else {
+      // Handle the case where results are not present
+      throw new Error('No data found')
+    }
+  } catch (error) {
+    // Throw the error to be handled by the caller
+    throw new Error(`Failed to fetch : ${error instanceof Error ? error.message : 'Unknown error'}`)
   }
 }
