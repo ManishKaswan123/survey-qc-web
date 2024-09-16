ec91d1f4d8b0069b1ac3b8bef136826bdefc2108
SatyamRana50 <194532@nith.ac.in>
2024-09-16 12:07:53 +0530

    status updated

--- src/sr/constants/status.ts
+++ src/sr/constants/status.ts
@@ -5,6 +5,7 @@ export type statusType =
   | 'resubmitted'
   | 'yetToStart'
   | 'rejected'
+  | 'inProgress'
 export const statusColors: Record<string, string> = {
   submitted: 'text-[#265B91]',
   approved: 'text-green-500',
@@ -12,6 +13,7 @@ export const statusColors: Record<string, string> = {
   resubmitted: 'text-orange-500',
   yetToStart: 'text-gray-500',
   rejected: 'text-purple-500',
+  inProgress: 'text-yellow-500',
 }
 export const statusMap = new Map<statusType, string>([
   ['submitted', 'Submitted'],
@@ -20,6 +22,7 @@ export const statusMap = new Map<statusType, string>([
   ['resubmitted', 'Resubmitted'],
   ['yetToStart', 'Yet to start'],
   ['rejected', 'Rejected'],
+  ['inProgress', 'In Progress'],
 ])
 export const statusObject = [
   {id: 'submitted', value: 'Submitted'},
@@ -28,6 +31,7 @@ export const statusObject = [
   {id: 'resubmitted', value: 'Resubmitted'},
   {id: 'yetToStart', value: 'Yet to start'},
   {id: 'rejected', value: 'Rejected'},
+  {id: 'inProgress', value: 'In Progress'},
 ]
 export const statusValues = Array.from(statusMap.values())
 export const statusKeys = Array.from(statusMap.values())
